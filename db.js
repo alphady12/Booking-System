f5672c52a57a67ffe6cd08ad5f5c4883dfbfeae0
scdy@gbox.ncf.edu.ph <158818151+alphady12@users.noreply.github.com>
2024-02-14 15:30:45 +0800

    Update db.js

--- db.js
+++ db.js
@@ -1,14 +1,14 @@
 // db.js
 const mysql = require('mysql2');
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
     host: 'sql6.freemysqlhosting.net',
     user: 'sql6683747',
     password: 'IcKlAGGWHm',
     database: 'sql6683747'
 });
 
-db.connect((err) => {
+db.getPool((err) => {
   if (err) {
     console.error("error connecting to mysql: ", err)
   } else {
